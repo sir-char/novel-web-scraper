0bf85b7b075298868cdb7b173b842d802884ec71
Chitwan Deol <chitwandeol@gmail.com>
2020-06-07 21:51:18 -0700

    Latest working progress update

--- novelWebScraping.py
+++ novelWebScraping.py
@@ -2,10 +2,14 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-url = "https://novelfull.com"
-response = requests.get(url)
+def getSoup(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup
 
-soup = BeautifulSoup(response.text, 'html.parser')
+url = "https://novelfull.com"
+
+soup = getSoup(url)
 
 posts = soup.find_all('div', class_="item")
 
@@ -26,14 +30,22 @@ with open("data.json", "r+") as data_file:
     data = json.load(data_file)
     for novel in data['novel']:
         new_url = f"{url}{novel['link']}"
-        response = requests.get(new_url)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = getSoup(new_url)
+        # last_page_li = soup.find('li', class_="last")
+        # print(last_page_li)
+        # last_page = soup.find('a')['data']
+        # last_page = int(soup.find('a')['data-*']) + 1
+        # print(f"last html page for {novel['title']} is page {last_page}")
         posts = soup.find_all('ul', class_="list-chapter")
+        novel['chapters'] = []
         for post in posts:
-            ch_title = post.find('a')['title']
-            ch_link = post.find('a')['href']
-            novel['chapters'] = {
-                "chapterTitle": ch_title,
-                "chapterLink": ch_link
-            }
+            for li in post.find_all('li'):
+                ch_title = li.find('a')['title']
+                ch_link = li.find('a')['href']
+                novel['chapters'].append({
+                    "chapterTitle": ch_title,
+                    "chapterLink": ch_link
+                })
+    data_file.seek(0)
     json.dump(data, data_file, indent=3)
+    # data_file.truncate()
